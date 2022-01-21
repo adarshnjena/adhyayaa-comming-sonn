5643aa98dd4b1816e04b8366b191b459b3047b82
adarshnjena <adarshnjena@gmail.com>
2022-01-20 23:52:37 -0800

    commit

--- src/components/adhyayaa_app.js
+++ src/components/adhyayaa_app.js
@@ -6,12 +6,23 @@ import Mobile from "./3D/Mobile";
 
 const Adhyaaya_app = () => {
   const style = {
-    width: "50vw",
+    width: "30vw",
     height: "100vh",
+    // zIndex: "-1",
   };
   return (
     <div className="adhyayaa_app">
-      <div style={style}></div>
+      <div className="appText">
+        <h1>hello world</h1>
+        <h3>
+          Here you can download 1080p x264 UHD, 1080p 60FPS, 1080p x265 10Bit,
+          4k HDR, 4k 2160p SDR 3D Movies through Google Drive Links.
+          High-quality movies with the best quality options and maximum
+          bitrates. We also focus on providing the best quality audio available.
+          4k HEVC Dolby Atmos is one of the best High-quality formats with low
+          file sizes.
+        </h3>
+      </div>
       <Canvas style={style}>
         <ambientLight intensity={0.5} />
         <OrbitControls></OrbitControls>
